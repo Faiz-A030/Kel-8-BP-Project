cfd36959fc1e6d217a8dbe537dfbc2a90a3f227a
Claudio Huge Siburian <claudiohuge26@gmail.com>
2025-06-24 19:30:31 +0700

    Claudio -112

--- Tic Tac Toe/Demo/GameComp.java
+++ Tic Tac Toe/Demo/GameComp.java
@@ -2,9 +2,9 @@ import java.awt.*;
 
 public class GameComp {
     // Background
-    public static final Color COLOR_BG = new Color(18, 32, 56);           // Biru Gelap
-    public static final Color COLOR_BG_STATUS = new Color(30, 46, 72);     // Biru Abu-abu
-    public static final Color COLOR_BG_SCORE = new Color(25, 55, 90);      // Biru Medium (sedikit lebih terang)
+    public static final Color COLOR_BG = new Color(30, 144, 255);           // Biru Terang (Dodger Blue)
+    public static final Color COLOR_BG_STATUS = new Color(65, 105, 225);     // Biru Medium (Royal Blue)
+    public static final Color COLOR_BG_SCORE = new Color(100, 149, 237);      // Biru Lembut (Cornflower Blue)
 
     // Font
     public static final Font FONT_STATUS = new Font("SansSerif", Font.BOLD, 16);
